115d4078d6aa583fc42553f456a4b56d33c12d02
rattisuk <nukobza@gmail.com>
2016-07-16 03:29:33 +0700

    Initial commit

---
+++ app/src/main/java/tech/test/surveys/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package tech.test.surveys.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tech.test.surveys.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
