3acc9fd0931b647fb36214b297c030d754158dfd
rattisuk <nukobza@gmail.com>
2016-07-16 12:42:25 +0700

    - add empty toolbar and remove old action bar - change colorPrimary to black

--- app/src/main/java/tech/test/surveys/activity/MainActivity.java
+++ app/src/main/java/tech/test/surveys/activity/MainActivity.java
@@ -2,16 +2,21 @@ package tech.test.surveys.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import tech.test.surveys.R;
 import tech.test.surveys.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
 
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -19,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
 
+    }
+    private void initInstances()
+    {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
