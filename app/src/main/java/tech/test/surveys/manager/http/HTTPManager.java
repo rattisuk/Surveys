beb9073df347e161891ec8697c9bab2f437059db
rattisuk <nukobza@gmail.com>
2016-07-18 02:07:24 +0700

    increase retrofit timeout to 60 seconds

--- app/src/main/java/tech/test/surveys/manager/http/HTTPManager.java
+++ app/src/main/java/tech/test/surveys/manager/http/HTTPManager.java
@@ -2,13 +2,25 @@ package tech.test.surveys.manager.http;
 
 import android.content.Context;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import tech.test.surveys.util.Contextor;
 
 public class HTTPManager {
-
+    private static final String BASE_URL = "https://www-staging.usay.co/app/";
+    private static final int CONNECT_TIMEOUT_SECOND = 60;
+    private static final int READ_TIMEOUT_SECOND = 60;
+    private static final int WRITE_TIMEOUT_SECOND = 60;
     private static HTTPManager instance;
+
+    OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+            .connectTimeout(CONNECT_TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .build();
 
     public static HTTPManager getInstance() {
         if (instance == null)
@@ -23,7 +35,8 @@ public class HTTPManager {
         mContext = Contextor.getInstance().getContext();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://www-staging.usay.co/app/")
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
