500c52f3c3576aa080e38e3101adb3c57da9fd35
rattisuk <nukobza@gmail.com>
2016-07-20 00:22:12 +0700

    fix bug menu item not change when rotate SecondFragment screen

--- app/src/main/java/tech/test/surveys/activity/MainActivity.java
+++ app/src/main/java/tech/test/surveys/activity/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
                     .commit();
         }
 
+        showVisibleMenu();
     }
 
     private void initInstances()
